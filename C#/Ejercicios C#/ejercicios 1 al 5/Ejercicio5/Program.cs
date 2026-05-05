533bc59392b32268ef04a7b41c5dc5a455842ca7
Stephen <stephencantu051223@gmail.com>
2026-05-05 03:47:33 -0500

    Agregando ejercicios en C#

---
+++ C#/Ejercicios C#/ejercicios 1 al 5/Ejercicio5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Depósito inicial: S/ ");
+            double saldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Meta a alcanzar: S/ ");
+            double meta = double.Parse(Console.ReadLine());
+
+            double tasa = 0.015;
+            int meses = 0;
+
+            Console.WriteLine($"Mes {meses}: S/ {saldo:F2}");
+
+            while (saldo < meta)
+            {
+                saldo = saldo * (1 + tasa);
+                meses++;
+                Console.WriteLine($"Mes {meses}: S/ {saldo:F2}");
+            }
+
+            Console.WriteLine($"Alcanzará la meta en {meses} meses.");
+        }
+
+    }
+}
+
